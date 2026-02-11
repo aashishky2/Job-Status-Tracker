5e877b9fe3d973b98c5abaf692999c312dbf78c8
Aashish Sharma <aashishky2@gmail.com>
2026-02-12 00:29:07 +0530

    Deleted changes

--- job_status-main/src/pages/dashboard/index.jsx
+++
@@ -1,7 +0,0 @@
-import AddJob from "./AddJob"
-import AllJobs from "./AllJobs"
-import Dashboard from "./Dashboard"
-import Profile from "./Profile"
-import SharedLayout from "./SharedLayout"
-
-export { AddJob, AllJobs, Dashboard, Profile, SharedLayout } 
